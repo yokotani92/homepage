b11e16b16871dc390859bfa59855bf7ee88915b5
Yuki Yokotani <yokotani.yuki@gmail.com>
2020-04-18 05:17:21 +0900

    Updates

---
+++ precache-manifest.9a54652f4c5f1ead8fecb5f1834e9372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efdf8d71b9e7797d745c9b93c404166",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "9c4bf11e43a86ecb5d70",
+    "url": "/homepage/static/js/2.fbab6491.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/homepage/static/js/2.fbab6491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db556a73f2cf279872f2",
+    "url": "/homepage/static/js/main.cdb25a43.chunk.js"
+  },
+  {
+    "revision": "0c8a22e9212c61d0b988",
+    "url": "/homepage/static/js/runtime-main.70a20e9b.js"
+  }
+]);
