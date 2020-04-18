824a9cb51ad62344938ded1566357efb2e4868e3
Yuki Yokotani <yokotani.yuki@gmail.com>
2020-04-19 04:02:07 +0900

    Updates

---
+++ precache-manifest.7c7b86fe923c7585665b37e4ca3ebfff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b09e090c77a7b1f028e6e6e8552f43",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "af72e92c259626c41e44",
+    "url": "/homepage/static/js/2.ace3f460.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/homepage/static/js/2.ace3f460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08003b25d1fc7f3fea0",
+    "url": "/homepage/static/js/main.d10756f9.chunk.js"
+  },
+  {
+    "revision": "0c8a22e9212c61d0b988",
+    "url": "/homepage/static/js/runtime-main.70a20e9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.93b4f35b338425d4bcc5003e79f709d6.js"
+  "/homepage/precache-manifest.7c7b86fe923c7585665b37e4ca3ebfff.js"
 );
 
 self.addEventListener('message', (event) => {
